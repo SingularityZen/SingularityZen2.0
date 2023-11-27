33fc47cdf32ab1a8ba3fdfa9f839095bf140fc10
Caleb Brewer <zenccb@calebs-air.mynetworksettings.com>
2023-11-26 21:17:45 -0500

    Successful HeroSection Import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Header';  // Adjust the path as per your project structure
 import Footer from './Footer';  // Adjust the path as per your project structure
-
+import HeroSection from './HeroSection';
 
 function App() {
   return (
     <div className="App" style={{ position: 'relative', minHeight: '100vh' }}>
       <Header />
+      <HeroSection />
       {/* You can add your main app content or other components here */}
       <Footer />
     </div>

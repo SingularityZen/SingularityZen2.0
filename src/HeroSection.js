ab75fb2f2b1f0f3cc8bb5124e6ec62cc95ef892f
Caleb Brewer <zenccb@calebs-air.mynetworksettings.com>
2023-11-26 21:17:04 -0500

    Removed Background Image Styling

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -1,29 +1,21 @@
 // HeroSection.js
 import React from 'react';
 
-const heroStyle = {
+const herostyle = {
     textAlign: 'center',
     padding: '50px 20px',
-    background: 'url(path_to_your_image.jpg) no-repeat center center', // Replace with your image path
+    background: 'url(https://hero.fandom.com/wiki/Heroes_Wiki:Hero?file=Hero.jpeg) no-repeat center center', // Replace with your image path
     backgroundSize: 'cover',
     color: 'white'
 };
 
 const HeroSection = () => {
     return (
-        <section style={heroStyle}>
+        <div >
             <h1>Welcome to Our App</h1>
             <p>Explore the world of cryptocurrency.</p>
-            <button style={{
-                padding: '10px 20px',
-                backgroundColor: '#007bff',
-                color: 'white',
-                border: 'none',
-                borderRadius: '5px',
-                cursor: 'pointer',
-                fontSize: '1rem'
-            }}>Get Started</button>
-        </section>
+            
+        </div>
     );
 };
 
